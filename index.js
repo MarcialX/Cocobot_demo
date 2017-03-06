2beae85abe019e0e143f3eb3f51a2f4b30dbf738
MarcialX <mbecerrilt92@gmail.com>
2017-03-05 22:37:28 -0600

    Hola

--- index.js
+++ index.js
@@ -8,15 +8,16 @@ app.use(bodyParser.json());
 app.listen((process.env.PORT || 5000));
 
 // Server frontpage
-app.get('/', function (req, res) {  
-    res.send('This is TestBot Server');
-});
+//app.get('/', function (req, res) {  
+//    res.send('This is TestBot Server');
+//});
 
 // Facebook Webhook
 app.get('/webhook', function (req, res) {  
-    if (req.query['hub.verify_token'] === process.env.VERIFY_TOKEN) {
+    if (req.query['hub.verify_token'] === cocobot_demo) {
         res.send(req.query['hub.challenge']);
     } else {
         res.send('Invalid verify token');
     }
+    res.send('This is TestBot Server');
 });
