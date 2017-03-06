2f85d4649564a48c441ec8f5b7ba66d509d93c9e
MarcialX <mbecerrilt92@gmail.com>
2017-03-05 21:52:28 -0600

    Hola

--- index.js
+++ index.js
@@ -1,32 +1,22 @@
-'use strict'
+var express = require('express');  
+var bodyParser = require('body-parser');  
+var request = require('request');  
+var app = express();
 
-const express = require('express')
-const bodyParser = require('body-parser')
-const request = require('request')
-const app = express()
+app.use(bodyParser.urlencoded({extended: false}));  
+app.use(bodyParser.json());  
+app.listen((process.env.PORT || 3000));
 
-app.set('port', (process.env.PORT || 5000))
+// Server frontpage
+app.get('/', function (req, res) {  
+    res.send('This is TestBot Server');
+});
 
-// Process application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({extended: false}))
-
-// Process application/json
-app.use(bodyParser.json())
-
-// Index route
-app.get('/', function (req, res) {
-    res.send('Hello world, I am a chat bot')
-})
-
-// for Facebook verification
-app.get('/webhook/', function (req, res) {
+// Facebook Webhook
+app.get('/webhook', function (req, res) {  
     if (req.query['hub.verify_token'] === 'cocobot_demo') {
-        res.send(req.query['hub.challenge'])
+        res.send(req.query['hub.challenge']);
+    } else {
+        res.send('Invalid verify token');
     }
-    res.send('Error, wrong token')
-})
-
-// Spin up the server
-app.listen(app.get('port'), function() {
-    console.log('running on port', app.get('port'))
-})
+});
